76e74fe6a24df85a8cc9bfb88c0933bcf381df27
Sami0189 <samisaimon28@gmail.com>
2024-10-29 22:08:49 +0600

    miles to kilometer

--- miles-to-kilometer-convert.js
+++ miles-to-kilometer-convert.js
@@ -1,18 +1,18 @@
 //1 mile = 1.60934 kilometer
 
-function mileToKilometer(mile){
-    const kilometer = mile * 1.60934; 
-    return kilometer;
-}
-const kilometerToMiles = mileToKilometer(75);
-console.log(kilometerToMiles);
+// function mileToKilometer(mile){
+//     const kilometer = mile * 1.60934; 
+//     return kilometer;
+// }
+// const kilometerToMiles = mileToKilometer(75);
+// console.log(kilometerToMiles);
 
 
 //again
 
-function kilometerToMiles(kilo){
-    const miles = kilo * 0.621371;
-    return miles;
-}
-const mile = kilometerToMiles(345);
-console.log(mile);
+// function kilometerToMiles(kilo){
+//     const miles = kilo * 0.621371;
+//     return miles;
+// }
+// const mile = kilometerToMiles(345);
+// console.log(mile);
