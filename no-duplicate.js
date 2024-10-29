c3a5edb6217cc2bfcfd7d97fa61fe390e4e9415d
Sami0189 <samisaimon28@gmail.com>
2024-10-30 02:30:12 +0600

    remove duplicate items from an array

---
+++ no-duplicate.js
@@ -0,0 +1,38 @@
+/**
+ * 
+ * array has some duplicate elements
+ * 
+ */
+
+// const biryaniKhor = ['abul','babul','cabul','abul','babul','dabul','kabul','cabul'];
+
+
+// function noDuplicate(array){
+//     const unique = [];
+//   for (const item of array){
+//         if(unique.includes(item)===false){
+//             unique.push(item);
+//         }
+//   }
+//   return unique;
+// }
+// const unique = noDuplicate(biryaniKhor);
+// console.log(unique);
+
+
+
+
+// const numbers = [1,2,3,4,1,32,2,3,6,4,5];
+
+// function num(number){
+//     const newArray = [];
+//        for(const nums of number){
+//            if(newArray.includes(nums)===false){
+//             newArray.push(nums);
+//            }
+//        }
+//        return newArray;
+// }
+
+// const newArray = num(numbers);
+// console.log(newArray);
